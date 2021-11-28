51618a3af522fc3c5e23ff421a9d5cd1e5c81298
enrique <nohunenrique@gmail.com>
2021-11-28 17:26:18 -0500

    Pokemon Api Con ReactNative

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import Pokemons from './components/Pokemons';
+import Details from './components/Details';
+// import Pokemons from './Components/Pokemons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const appNavigator = createStackNavigator({
+  
+  Details: {
+    screen: Details,
   },
-});
+  Home: {
+    screen: Pokemons,
+  }, 
+  },
+  {
+    initialRouteName: 'Home'
+  },
+  );
+
+  const AppContainer = createAppContainer(appNavigator);
+  export default class App extends Component {
+    render() {
+      return <AppContainer />;
+    }
+  }
+  
+  // export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
